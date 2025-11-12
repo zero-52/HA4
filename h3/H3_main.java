0a0b6e786bfb959792935b132a466286773d7831
zero-52 <yatin.zhang@outlook.com>
2025-11-13 05:22:28 +0800

    HA4

--- h3/H3_main.java
+++ h3/H3_main.java
@@ -3,9 +3,11 @@ package h3;
 public class H3_main {
 	public static void main (String [] args) {
 		float celsiusFloat = 16.8f;
-		float fahrenheitFloat = celsiusFloat * 9/5 + 32f;
+		float fahrenheitFloat = 88.9f;
+		fahrenheitFloat =celsiusFloat * 9/5 + 32f;
 		double celsiusDouble = 16.8;
-		double fahrenheitDouble = celsiusDouble * 9/5 + 32;
+		double fahrenheitDouble = 155.86;
+		fahrenheitDouble = celsiusDouble * 9/5 + 32;
 		
 		System.out.println("Das Wetter heute liegt bei " + celsiusFloat + " Grad Celsius und " + fahrenheitFloat + " Grad Fahrenheit.");
 		System.out.println("Das Wetter heute liegt bei " + celsiusDouble + " Grad Celsius und " + fahrenheitDouble + " Grad Fahrenheit.");
