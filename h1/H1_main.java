3933f02e683021595f8d439ed5878136de080968
zero-52 <yatin.zhang@outlook.com>
2025-11-15 00:30:59 +0800

    HA4

--- h1/H1_main.java
+++ h1/H1_main.java
@@ -1,41 +1,52 @@
 package h1;
 
 public class H1_main {
-	public static void main (String [] args ) {
-		int zone = 4;
-		double price = 2;
+	public static void main(String[] args) {
+		double guthaben = -150;
+		double monEingang = 50;
+		int rating = -2;
+		boolean warnhinweis = true;
+		boolean negativ = true;
 		
-		switch (zone) {
-		case 1:
-			price = price;
-			System.out.println(price);
-			break;
-		case 2:
-			price = 2.35;
-			System.out.println(price);
-			break;
-		case 3:
-			price = 2.85;
-			System.out.println(price);
-			break;
-		case 4:
-			price = 2.85;
-			System.out.println(price);
-			break;
-		case 5:
-			price = 3.55;
-			System.out.println(price);
-			break;
-		case 6:
-			price = 4;
-			System.out.println(price);
-		    break;
-		   
-		default:
-			price = 4;
-			System.out.println(price);
-		
+		if (guthaben < 0) {
+			negativ = true;
 		}
-	}
+		
+			else {
+				negativ = false;
+			}
+		
+		if (guthaben > 0) {
+				rating += 3;
+			}
+		
+		else if (guthaben == 0) {
+			rating += 2;
+		}
+		
+		
+		if (guthaben < 0 && monEingang >= Math.abs(guthaben)) {
+			rating += 1;
+		}
+		
+		   else if (guthaben < 0 && monEingang < Math.abs(guthaben)) {
+			rating -= 1;
+		}
+		
+		if (guthaben < 0 && monEingang < Math.abs(guthaben) && rating < 0) {
 
+			warnhinweis = true;
+		}  
+		   else {
+			warnhinweis = false;
+			}
+		
+				
+	System.out.println(negativ);
+	       System.out.println(rating);
+	System.out.println(warnhinweis);
+	//Testfall 4,5 und 7 teilweise falsch: noch mal nachfragen vielleicht
+	}
 }
+	
+
