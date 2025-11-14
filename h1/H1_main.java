3b5d5709fe9446c0013bb9863399635dd57b34b8
zero-52 <yatin.zhang@outlook.com>
2025-11-15 00:31:53 +0800

    HA4

--- h1/H1_main.java
+++ h1/H1_main.java
@@ -2,51 +2,44 @@ package h1;
 
 public class H1_main {
 	public static void main(String[] args) {
-		double guthaben = -150;
-		double monEingang = 50;
-		int rating = -2;
-		boolean warnhinweis = true;
-		boolean negativ = true;
-		
-		if (guthaben < 0) {
-			negativ = true;
-		}
-		
-			else {
-				negativ = false;
-			}
-		
-		if (guthaben > 0) {
-				rating += 3;
-			}
-		
-		else if (guthaben == 0) {
-			rating += 2;
-		}
-		
-		
-		if (guthaben < 0 && monEingang >= Math.abs(guthaben)) {
-			rating += 1;
-		}
-		
-		   else if (guthaben < 0 && monEingang < Math.abs(guthaben)) {
-			rating -= 1;
-		}
-		
-		if (guthaben < 0 && monEingang < Math.abs(guthaben) && rating < 0) {
+		int zone = 82;
+		double price = 8;
 
-			warnhinweis = true;
-		}  
-		   else {
-			warnhinweis = false;
+		switch (zone) {
+		case 1:
+			price = 2;
+			break;
+			
+		case 2:
+			price = 2.35;
+			break;
+			
+		case 3:
+		case 4:
+			price = 2.85;
+			break;
+			
+		case 5:
+			price = 3.55;
+			break;
+			
+		case 6:
+			price = 4;
+			break;
+
+		default:
+			if (zone >= 6) {
+				price = 4;
 			}
-		
-				
-	System.out.println(negativ);
-	       System.out.println(rating);
-	System.out.println(warnhinweis);
-	//Testfall 4,5 und 7 teilweise falsch: noch mal nachfragen vielleicht
-	}
-}
+			else {
+				price = 0;
+			}
+			
 	
 
+		}			
+		System.out.println(price);
+
+	}
+
+}
